236fe49424cd15acb35037a5932d033babb90837
vladharkevic <Harkevicvlad16@gmail.com>
2025-07-04 08:45:05 +0200

    Add files via upload

---
+++ Application.php
@@ -0,0 +1,23 @@
+<?php // Alonity Framework Config | Updated: 18.05.2020 11:53:56
+
+return array (
+  'app_name' => 'UCave',
+  'app_version' => '1.0.0',
+  'app_author' => 'Qexy',
+  'app_site' => 'https://qexy.org',
+  'app_description' => 'UCave - Проект серверов Minecraft',
+  'app_filename' => '/Applications/UCave/UCave.php',
+  'app_namespace' => 'App',
+  'app_class' => 'UCave',
+  'app_method' => 'execute',
+  'default_route_methods' => 
+  array (
+    0 => 'GET',
+  ),
+  'app_config_filename' => '/Applications/UCave/Config.php',
+  'default_public_dir' => '/Public/Themes/Default',
+  'l10n_dir' => '/Applications/UCave/l10n',
+  'templater_cache' => false,
+);
+
+?>
